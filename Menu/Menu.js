1cf62a7d828d90c17d21dcdd04faf2451e0bd7a4
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-07-10 12:17:41 -0700

    changes from first round. never committed

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,6 @@
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu reference. 
   menu.classList.toggle('menu--open');
 }
 
@@ -14,3 +14,6 @@ console.log(menuButton);
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+
+// document.querySelectorAll('ul').appendChild(document.createElement('li', 'testtttttttttting'));
